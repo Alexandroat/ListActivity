ed752f011b17291c7aa8e410cc337e1e9b32f304
alejandro <alejandro@>
2014-12-02 12:46:09 +0100

    commit 5 practica 04 terminada

--- ListActivity/src/com/example/listactivity/MainActivity.java
+++ ListActivity/src/com/example/listactivity/MainActivity.java
@@ -9,6 +9,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ListActivity;
@@ -17,9 +18,11 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,25 +31,17 @@ public class MainActivity extends ListActivity {
 	protected String mBlogPostTitles[];
 	public final static String TAG = MainActivity.class.getSimpleName();
 	private TextView textview;
-	private ArrayAdapter<String> adapter;
+	// private ArrayAdapter<String> adapter;
 	private GetBlogPosTask getBlogPosTask = new GetBlogPosTask();
+	public final static int NUMBER_OF_POSTS = 20;
+	private JSONObject mBlogData;
+	final static String URL_JSON = "http://itvocationalteacher.blogspot.com/feeds/posts/default?alt=json";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		textview = (TextView) findViewById(R.id.textView1);
-		// mBlogPostTitles =
-		// getResources().getStringArray(R.array.android_names);
-
-		
-		 adapter = new ArrayAdapter<String>(this,
-		 android.R.layout.simple_list_item_1, mBlogPostTitles);
-		 setListAdapter(adapter);
-		 
-		if (mBlogPostTitles != null) {
-			textview.setVisibility(textview.INVISIBLE);
-		}
 
 		if (isNetworkAvailable()) {
 			getBlogPosTask.execute();
@@ -54,6 +49,42 @@ public class MainActivity extends ListActivity {
 			Toast.makeText(this, R.string.no_connection_message,
 					Toast.LENGTH_LONG).show();
 
+		}
+		if (mBlogPostTitles != null) {
+			textview.setVisibility(textview.INVISIBLE);
+		}
+	}
+
+	public void updateList() {
+		if (mBlogData != null) {
+			// TODO : Manejar errores
+			try {
+				// JSONObject jsonObject = new JSONObject(text);
+				// jsonObject.getString("version");
+				JSONObject jsonFeed = mBlogData.getJSONObject("feed");
+				JSONArray jsonAentry = jsonFeed.getJSONArray("entry");
+				mBlogPostTitles = new String[jsonAentry.length()];
+
+				for (int i = 0; i < jsonAentry.length(); i++) {
+					JSONObject jsonPost = (JSONObject) jsonAentry.get(i);
+					JSONObject jsonTitle = (JSONObject) jsonPost.get("title");
+
+					mBlogPostTitles[i] = Html.fromHtml(
+							jsonTitle.getString("$t")).toString();
+				
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+							this,
+							android.R.layout.simple_expandable_list_item_1,
+							mBlogPostTitles);
+					setListAdapter(adapter);
+					findViewById(R.id.textView1).setVisibility(View.INVISIBLE);
+				}
+
+			} catch (JSONException e) {
+				Log.e(TAG, "exception caught:", e);
+			}
+		} else {
+			// Log.e(TAG, mBlogData.toString());
 		}
 
 	}
@@ -67,31 +98,23 @@ public class MainActivity extends ListActivity {
 		}
 		return isAvailable;
 	}
-	
-	class GetBlogPosTask extends AsyncTask<Object, Object, Object> {
-		public final static int NUMBER_OF_POSTS = 20;
-		
-		final static String URL_JSON = "http://itvocationalteacher.blogspot.com/feeds/posts/default?alt=json";
+
+	class GetBlogPosTask extends AsyncTask<Object, Void, JSONObject> {
 
 		// public GetBlogPosTask()
 
 		@Override
-		protected Object doInBackground (Object... params) {
+		protected JSONObject doInBackground(Object[] params) {
 			int responseCode = -1;
 			URL blogFeedUrl = null;
+			JSONObject jsonResponse = null;
 
 			try {
 				blogFeedUrl = new URL(URL_JSON);
 
 				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl
 						.openConnection();
-				JSONObject jsonObject;
-				JSONObject jsonFeed;
-				JSONArray jsonAentry;
-				JSONObject mBlogData;
-				
-				//String test = "";
-				
+
 				if (connection != null) {
 					connection.connect();
 				}
@@ -105,30 +128,15 @@ public class MainActivity extends ListActivity {
 					StringBuilder stringBuilder = new StringBuilder();
 					String lines;
 					String text = "";
-					
+
 					while ((lines = bufferedReader.readLine()) != null) {
 
 						stringBuilder = stringBuilder.append(lines);
 						text = stringBuilder.toString();
+						jsonResponse = new JSONObject(text);
 
 					}
-					
-					//Log.i(TAG, "Json: " + text);
-					jsonObject = new JSONObject(text);
-					//jsonObject.getString("version");
-					jsonFeed = jsonObject.getJSONObject("feed");
-					jsonAentry = jsonFeed.getJSONArray("entry");
-					
-					for (int i = 0; i < jsonAentry.length(); i++) {
-						JSONObject jsonPost = (JSONObject) jsonAentry.get(i);
-	                    JSONObject jsonTitle = (JSONObject) jsonPost.get("title");
-	                    mBlogPostTitles = new String[jsonTitle.length()];
-	                    //String title = Html.escapeHtml(jsonTitle.getString("$t"));
-	                     mBlogPostTitles[i] = jsonTitle.getString("$t").toString();
-	                   //Log.i(TAG, "Titulosss: " + test);
-					}
-					
-					
+
 				} else {
 					Log.e(TAG, "Unable to connect : " + responseCode);
 				}
@@ -142,11 +150,15 @@ public class MainActivity extends ListActivity {
 			} catch (Exception e) {
 				System.err.println("Error");
 			}
-			return mBlogPostTitles;
+			return jsonResponse;
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			mBlogData = result;
+			updateList();
 		}
 
 	}
 
 }
-
-
